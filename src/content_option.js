682f6e4851e82a797ec6233abf4e6db11b2414db
mintikisi <comx_kurniawan@yahoo.com>
2024-07-31 09:11:54 +0700

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -1,15 +1,15 @@
 const logotext = "JOHN";
 const meta = {
-    title: "John Doe",
+    title: "MINTIKISHI",
     description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m MINTIKISHI",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        first: "I love PARLAY",
+        second: "I LOVE LAIF TOO",
+        third: "I develop FOR MY LOVE LATIF",
     },
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
